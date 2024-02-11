8329091155c0bed52224a6ff41122e5706bedaee
Attila GOLONCSER <agl@kxxp.net>
2024-02-11 17:05:11 +0100

    Copy with sudo

--- src/sshd_configure.sh
+++ src/sshd_configure.sh
@@ -3,17 +3,17 @@
 # Add initial config when it is not already there: 
 if [ -e /etc/ssh/sshd_config ] ; then
     grep 'scripts-init-server$' /etc/ssh/sshd_config || {
-        cp share/sshd_config /etc/ssh/sshd_config
+        sudo cp share/sshd_config /etc/ssh/sshd_config
     }
 else 
-    cp share/sshd_config /etc/ssh/sshd_config
+    sudo cp share/sshd_config /etc/ssh/sshd_config
 fi
 
 # Trust user CA when is not already there: 
 if [ -e /etc/ssh/TrustedUserCAKeys ] ; then
     grep 'scripts-init-server$' /etc/ssh/TrustedUserCAKeys || {
-        cp share/TrustedUserCAKeys /etc/ssh/TrustedUserCAKeys
+        sudo cp share/TrustedUserCAKeys /etc/ssh/TrustedUserCAKeys
     }
 else 
-    cp share/TrustedUserCAKeys /etc/ssh/TrustedUserCAKeys
+    sudo cp share/TrustedUserCAKeys /etc/ssh/TrustedUserCAKeys
 fi
