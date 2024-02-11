136fe8c37eec9cd00e0639f904f13a78092bf411
Attila GOLONCSER <agl@kxxp.net>
2024-02-11 14:06:09 +0100

    ci: Add INSTALL_ONLY flag

--- src/sshd_enable.sh
+++ src/sshd_enable.sh
@@ -2,14 +2,24 @@
 
 set -eu
 
+_install_only(){
+    if [ -n "$INSTALL_ONLY" ] ; then
+        echo "WARNING: INSTALL_ONLY flag set, exiting."
+        exit 0
+    fi
+}
+
+
 echo "Enable sshd..."
 case $(uname -s) in
     Linux)
+        _install_only
         systemctl enable sshd
         systemctl restart sshd
         ;;
     FreeBSD)
         sysrc sshd_enable="YES"
+        _install_only
         service sshd restart
         ;;
      *)
