cd1889fae2765eb3cfe9865069f10eb6c04f5436
Attila GOLONCSER <agl@kxxp.net>
2024-02-11 14:13:48 +0100

    Early exit

--- src/sshd_enable.sh
+++ src/sshd_enable.sh
@@ -1,25 +1,22 @@
 #!/bin/sh
+
 set -eu
 
 INSTALL_ONLY=${INSTALL_ONLY:=""}
-_install_only(){
-    if [ -n "$INSTALL_ONLY" ] ; then
-        echo "WARNING: INSTALL_ONLY flag set, exiting."
-        exit 0
-    fi
-}
+if [ -n "$INSTALL_ONLY" ] ; then
+    echo "WARNING: INSTALL_ONLY flag set, exiting."
+    exit 0
+fi
 
 
 echo "Enable sshd..."
 case $(uname -s) in
     Linux)
-        _install_only
         systemctl enable sshd
         systemctl restart sshd
         ;;
     FreeBSD)
         sysrc sshd_enable="YES"
-        _install_only
         service sshd restart
         ;;
      *)
